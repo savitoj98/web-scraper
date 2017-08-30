a311df2686fa3da49f29559308b34602a8a280f9
phoenix157 <savitojsingh15@gmail.com>
2017-08-30 18:43:37 +0530

    web-scraper commit one

---
+++ web_scraper_product_info.py
@@ -0,0 +1,59 @@
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+#works on-'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
+my_url = ''
+
+uClient = uReq(my_url)  #requests all the data from my_url
+page_html = uClient.read()  #pastes all the raw html in a variable
+uClient.close()   #since it is opened client close it 
+
+
+page_soup = soup(page_html, "html.parser")
+
+ 
+containers = page_soup.findAll("div", {"class":"item-container"}) 
+
+
+filename = "products.csv"   
+f=open(filename, "w")
+headers = "brand, product_name, shipping, price\n"
+
+f.write(headers)
+
+for container in containers:
+	
+
+	title_container = container.findAll("a", {"class":"item-title"}) 
+	product_name = title_container[0].text   #title
+
+	try:
+		brand = container.div.div.a.img["title"]   
+	except:
+		brand = product_name.split(' ')[0]
+
+
+	shipping_container = container.findAll('li', {"class":"price-ship"})  
+	shipping = shipping_container[0].text.strip()  #remove whitespace char
+
+	if shipping == '':
+		shipping = 'Info not available'
+
+	price_container = container.findAll("li", {"class":"price-current"})
+	price_list = price_container[0].text.split()
+
+	if price_list[0] == '|':
+		price = price_list[1]
+	else:
+		price = price_list[0]
+
+
+	print("brand:" + brand)
+	print("product name:" + product_name)
+	print("shipping:" + shipping)
+	print("price:"+ price)
+
+	f.write(brand + "," + product_name.replace(",","|") + ", "+ shipping + "," + price + "\n")
+
+f.close()
